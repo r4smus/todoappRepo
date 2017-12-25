cfc587049891990bbfc174ba5142ee2fbcf15644
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-12-25 12:47:10 +0100

    Created database configuration, model, repository and controller

---
+++ todoapp_backend/src/main/java/com/example/todoapp/models/Todo.java
@@ -0,0 +1,73 @@
+package com.example.todoapp.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+
+
+
+@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)
+@Entity
+public class Todo {
+    @Id
+    private String id;
+    
+    @NotBlank
+    @Size(max=100)
+    private String title;
+    
+    private Boolean completed = false;
+    
+    private Date createdAt = new Date();
+    
+    public Todo() {
+        super();
+    }
+    
+    public Todo(String title) {
+        this.title = title;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public Boolean getCompleted() {
+        return completed;
+    }
+    
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+    
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format(
+                "Todo[id=%s, title='%s', completed='%s']",
+                id, title, completed);
+    }
+}
---
+++ todoapp_backend/src/main/java/com/example/todoapp/repositories/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.todoapp.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.todoapp.models.Todo;
+
+@Transactional
+public interface TodoRepository extends CrudRepository<Todo, String>  {
+
+}
